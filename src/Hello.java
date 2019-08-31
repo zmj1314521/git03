8e9f41e34adc72a8e5745289cf3d6cf5e7117b31
zmj1314521 <790284781@qq.com>
2019-08-31 23:14:40 +0800

    hhh

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,8 @@
  */
 public class Hello {
     public static void main(String [] args){
-        System.out.println("hhh");
+
+        System.out.println("hhhssss");
+        System.out.println("hhhssss");
     }
 }
